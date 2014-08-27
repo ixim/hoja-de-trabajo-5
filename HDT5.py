0cad0747a72941afd8555d166c3978c36278bf41
ixim <ixim_21@hotmail.com>
2014-08-27 14:30:46 -0600

    arreglando

--- HDT5.py
+++ HDT5.py
@@ -13,52 +13,38 @@ import random
 INTERVALO_PROCESOS = 10#tiempo creacion de procesos de forma exponencial
 RAMDOM_SEED = 3
 
-def inicio(env,procesador,intervalo):
+def inicio(env,RAMtotal,procesador,intervalo):
     
     for i in range(10):
         instrucciones = random.randint(1,10)#se asigna la cant de instrucciones q tiene el proceso
-        c = new(env,procesador,instrucciones,'Proceso%02d' % i)
+        c = new(env,RAMtotal,procesador,instrucciones,'Proceso%02d' % i)
         env.process(c)
         t = random.expovariate(1.0/intervalo)#tiempo en q aparece otro proceso
         yield env.timeout(t)#se espera hasta tiempo t para otro proceso
     
-def new (env,procesador,instrucciones,name):
-    global RAMtotal,RAMnuevo
+def new (env,RAMtotal,procesador,instrucciones,name):
+    global RAMnuevo,arrive
     RAMnuevo = random.randint(1,10)#numero aleatorio del ram requerido por proceso
-
-    RAMtotal.get(RAMnuevo)
-    print('RAM libre: %7.4f %s RAM asignado:%7.4f Instrucciones:%7.4f' % (RAMtotal, name,RAMnuevo,instrucciones))
-    """
-    with RAMtotal(RAMnuevo) as req:
+    
+    with RAMtotal.get(RAMnuevo) as req:
+        #print('%7.4f %s: Termina tiempo de espera' % (env.now, name))
         yield req
-        print('RAM libre: %7.4f %s RAM asignado:%7.4f Instrucciones:%7.4f' % (RAMtotal.level, name,RAMnuevo,instrucciones))
-)
-"""
-    if RAMtotal > RAMnuevo:#si el ram nuevo asignado es menor al ram restante, se puede realizar 
-        env.process(ready(env,procesador,instrucciones,name))
-    else:
-        with procesador.request() as req:  #pedimos conectarnos al
-            tiempo = random.uniform(1, 10)#ram
-            yield req
 
-            tib = random.expovariate(1.0 / 10)
-            yield env.timeout(tib)#se espera el tiempo asignado a tib
-            print('%7.4f %s: Termina tiempo de espera' % (env.now, name))
-            env.process(ready(env,procesador,instrucciones,name))
+    print('RAM libre: %7.4f %s RAM asignado:%7.4f Instrucciones:%7.4f' % (RAMtotal.level, name,RAMnuevo,instrucciones))
+    arrive = env.now
+    env.process(ready(env,procesador,instrucciones,name))
 
 def ready(env,procesador,instrucciones,name):
-    global wait,totalwait,RAMtotal,RAMnuevo
+    global wait,totalwait#,RAMnuevo
 
-    arrive = env.now
-    print('Tiempo %7.4f' % (arrive))
-
-    while instrucciones > 3:
-        instrucciones = instrucciones - 3#se resta la cant de instrucciones q hace la CPU por unidad de tiempo
-        print('instrucciones: %7.4f' % (instrucciones))
+    with procesador.request() as reqProcesador:  #pedimos conectarnos al
+        yield reqProcesador
         yield env.timeout(1)
 
-    RAMtotal = RAMtotal + RAMnuevo
-    print('RAM reasignado: %7.4f' % (RAMnuevo))
+        while instrucciones > 3:
+            instrucciones = instrucciones - 3#se resta la cant de instrucciones q hace la CPU por unidad de tiempo
+            print('instrucciones: %7.4f' % (instrucciones))
+
     wait = env.now - arrive
     totalwait = totalwait + wait
 
@@ -72,7 +58,7 @@ totalwait = 0
 procesador = simpy.Resource(env, capacity=1)
 RAMtotal = simpy.Container(env, init=100, capacity=100)
 #waiting = simpy.Resource(env, capacity=1)
-env.process(inicio(env, procesador, INTERVALO_PROCESOS))
+env.process(inicio(env,RAMtotal,procesador, INTERVALO_PROCESOS))
 env.run()
 
 print 'tiempo total de espera', totalwait, 'promedio: ',totalwait / 5.0
